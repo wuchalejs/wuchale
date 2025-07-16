c012a5b7721fdcde5264a74a47f3cb759258998e
W2Wizard <main@w2wizard.dev>
2025-07-16 13:29:01 +0200

    Double check the local
    If I were to go to /test instead of /[locale]/test, it would fail to import.

--- examples/sveltekit/src/hooks.server.ts
+++ examples/sveltekit/src/hooks.server.ts
@@ -1,14 +1,16 @@
 import type { Handle } from '@sveltejs/kit'
 import { initRegistry } from 'wuchale/runtime'
 
+const locales = ['en', 'es', 'fr'];
 const runWithCatalog = await initRegistry()
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const locale = event.params.locale ?? 'en';
-	const catalog = await import(`./locales/${locale}.svelte.js`);
+	const validLocale = locales.find(l => l === locale) ? locale : 'en';
+	const catalog = await import(`./locales/${validLocale}.svelte.js`);
 	return await runWithCatalog(catalog, () =>
 		resolve(event, {
-			transformPageChunk: ({ html }) => html.replace('%sveltekit.lang%', locale)
+			transformPageChunk: ({ html }) => html.replace('%sveltekit.lang%', validLocale)
 		})
 	);
 }
