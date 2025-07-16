d15f49e07a30baab90c0c90c58f03d13e4f52a19
W2Wizard <main@w2wizard.dev>
2025-07-16 13:16:42 +0200

    Update hooks.server.ts

--- examples/sveltekit/src/hooks.server.ts
+++ examples/sveltekit/src/hooks.server.ts
@@ -6,24 +6,11 @@ const runWithCatalog = await initRegistry()
 const locales = ['en', 'es', 'fr']
 
 export const handle: Handle = async ({ event, resolve }) => {
-    const locale = locales.find(l => l === event.params.locale)
-    if (!locale) {
-        return await resolve(event, {
-		transformPageChunk: ({ html }) => {
-			if (html.includes('%sveltekit.lang%')) {
-				return html.replace('%sveltekit.lang%', 'en');
-			}
-			return html;
-		}
-	})
-    }
-    const catalog = await import(`./locales/${locale}.svelte.js`)
-    return await runWithCatalog(catalog, async () => await resolve(event, {
-		transformPageChunk: ({ html }) => {
-			if (html.includes('%sveltekit.lang%')) {
-				return html.replace('%sveltekit.lang%', locale);
-			}
-			return html;
-		}
-	}))
+	const locale = event.params.locale ?? 'en';
+	const catalog = await import(`./locales/${locale}.svelte.js`);
+	return await runWithCatalog(catalog, () =>
+		resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%sveltekit.lang%', locale)
+		})
+	);
 }
