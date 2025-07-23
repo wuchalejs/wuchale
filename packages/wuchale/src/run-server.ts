2689f0ab7d9292117bc02f1362d78205a8655a55
K1DV5 <kidusadugna@gmail.com>
2025-07-23 14:58:15 +0200

    no need for async in run-server functions

--- packages/wuchale/src/run-server.ts
+++ packages/wuchale/src/run-server.ts
@@ -1,7 +1,7 @@
 import { AsyncLocalStorage } from 'node:async_hooks'
 import { Runtime, type CatalogModule } from './runtime.js'
 
-type LoaderFunc = (locale: string) => CatalogModule | Promise<CatalogModule>
+type LoaderFunc = (locale: string) => CatalogModule
 
 const catalogCtx: AsyncLocalStorage<{[key: string]: Runtime}> = new AsyncLocalStorage()
 
@@ -12,10 +12,12 @@ export function registerLoader(key: string, load: LoaderFunc): () => Runtime {
     return () => catalogCtx.getStore()[key] ?? new Runtime()
 }
 
-export async function runWithLocale<T>(locale: string, func: () => T | Promise<T>): Promise<T> {
+export function runWithLocale<T>(locale: string, func: () => T): T {
     const catalogs: {[key: string]: Runtime} = {}
     for (const [key, load] of Object.entries(catalogModuleLoaders)) {
-        catalogs[key] = new Runtime(await load(locale))
+        catalogs[key] = new Runtime(load(locale))
     }
-    return await catalogCtx.run(catalogs, func)
+    return catalogCtx.run(catalogs, func)
 }
+
+export const _w_rt_ = (key: string) => catalogCtx.getStore()?.[key] ?? new Runtime()
