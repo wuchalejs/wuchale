3a4ee89be68ac144ca3e2068791a36a49d187c94
K1DV5 <kidusadugna@gmail.com>
2025-08-16 10:56:53 +0200

    add currentRT function to server load util

--- packages/wuchale/src/load-utils/server.ts
+++ packages/wuchale/src/load-utils/server.ts
@@ -1,10 +1,14 @@
 import type { LoaderFunc } from './index.js'
-import { Runtime } from '../runtime.js'
+import { Runtime, type CatalogModule } from '../runtime.js'
 import { AsyncLocalStorage } from 'node:async_hooks'
 
-type LoadedCatalogs = Record<string, Record<string, Runtime>>
+type LoadedCatalogs = Record<string, Record<string, CatalogModule>>
 const catalogs: Record<string, LoadedCatalogs> = {}
 const catalogCtx: AsyncLocalStorage<LoadedCatalogs> = new AsyncLocalStorage()
+
+export function currentRT(key: string, loadID: string) {
+    return new Runtime(catalogCtx.getStore()[key][loadID])
+}
 
 export async function loadLocales(key: string, loadIDs: string[], load: LoaderFunc, locales: string[]): Promise<(loadID: string) => Runtime> {
     if (loadIDs == null) {
@@ -19,10 +23,10 @@ export async function loadLocales(key: string, loadIDs: string[], load: LoaderFu
             loaded[key] = {}
         }
         for (const id of loadIDs) {
-            loaded[key][id] = new Runtime(await load(id, locale))
+            loaded[key][id] = await load(id, locale)
         }
     }
-    return (loadID: string) => catalogCtx.getStore()?.[key]?.[loadID] ?? new Runtime()
+    return (loadID: string) => currentRT(key, loadID)
 }
 
 export async function runWithLocale<T>(locale: string, func: () => T): Promise<T> {
