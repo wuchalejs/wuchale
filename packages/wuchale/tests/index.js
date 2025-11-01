ec96bea6c19b5cbee5959aa2bd5e1c6115a4d2ca
K1DV5 <kidusadugna@gmail.com>
2025-11-01 18:18:46 +0100

    improve url module exports

--- packages/wuchale/tests/index.js
+++ packages/wuchale/tests/index.js
@@ -313,9 +313,9 @@ test('URL matcher', t => {
             ["/en","/es"]
         ],
     ], ['en', 'es'])
-    t.assert.deepEqual(matcher(new URL('http://foo.js/')), {path: '/', locale: null, alternates: {}})
-    t.assert.deepEqual(matcher(new URL('http://foo.js/en/foo')), {path: '/foo', locale: 'en', alternates: { en: '/en/foo', es: '/es/foo' }})
-    t.assert.deepEqual(matcher(new URL('http://foo.js/en')), {path: '/', locale: 'en', alternates: { en: '/en', es: '/es' }})
-    t.assert.deepEqual(matcher(new URL('http://foo.js/es/')), {path: '/', locale: 'es', alternates: { en: '/en', es: '/es' }})
-    t.assert.deepEqual(matcher(new URL('http://foo.js/es/ruta')), {path: '/path', locale: 'es', alternates: { en: '/en/path', es: '/es/ruta' }})
+    t.assert.deepEqual(matcher(new URL('http://foo.js/')), {path: '/', locale: null, altPatterns: {}, params: {}})
+    t.assert.deepEqual(matcher(new URL('http://foo.js/en/foo')), {path: '/foo', locale: 'en', altPatterns: { en: '/en/*rest', es: '/es/*rest' }, params: {rest: 'foo'}})
+    t.assert.deepEqual(matcher(new URL('http://foo.js/en')), {path: '/', locale: 'en', altPatterns: { en: '/en', es: '/es' }, params: {}})
+    t.assert.deepEqual(matcher(new URL('http://foo.js/es/')), {path: '/', locale: 'es', altPatterns: { en: '/en', es: '/es' }, params: {}})
+    t.assert.deepEqual(matcher(new URL('http://foo.js/es/ruta')), {path: '/path', locale: 'es', altPatterns: { en: '/en/path', es: '/es/ruta' }, params: {}})
 })
