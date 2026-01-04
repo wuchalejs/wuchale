bb64135df743e96f6401fe4631b8c556b5955cf8
K1DV5 <kidusadugna@gmail.com>
2026-01-04 17:05:29 +0100

    add pre-commit hook to run biome check

---
+++ scripts/fmt.ts
@@ -0,0 +1,24 @@
+// $ node %f
+import { exec } from 'node:child_process'
+
+const ecmd = (cmd: string, print = true): Promise<string> =>
+    new Promise(res => {
+        exec(cmd, (err, sout, serr) => {
+            if (err) {
+                console.error(serr.trim())
+                process.exit(1)
+            }
+            sout = sout.trim()
+            print && sout && console.log(sout)
+            res(sout)
+        })
+    })
+
+const staged = await ecmd('git diff --staged --diff-filter=ACM --name-only', false)
+
+if (staged) {
+    await ecmd('npm run fmt')
+    for (const file of staged.split('\n')) {
+        await ecmd(`git add ${file}`)
+    }
+}
