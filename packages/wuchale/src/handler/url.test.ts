199bdedfa99ed14aaf9dedd9f6778f0c3c3af2ce
K1DV5 <kidusadugna@gmail.com>
2026-03-28 17:04:46 +0100

    update dependencies

--- packages/wuchale/src/handler/url.test.ts
+++ packages/wuchale/src/handler/url.test.ts
@@ -5,8 +5,7 @@ import { patternFromTranslate, patternToTranslate } from './url.js'
 
 test('Pattern to/fro', t => {
     t.assert.equal(patternToTranslate('/foo/*rest'), '/foo/{0}')
-    // for some reason pathToRegexp repeats :bar resulting in 2, but it doesn't break anything
-    t.assert.equal(patternToTranslate('/foo/:bar/baz{/*rest}'), '/foo/{2}/baz/{1}')
+    t.assert.equal(patternToTranslate('/foo/:bar/baz{/*rest}'), '/foo/{0}/baz/{1}')
     t.assert.equal(patternFromTranslate('/foo/{0}/baz', [{ name: 'bar', type: 'param' }]), '/foo/:bar/baz')
     t.assert.equal(
         patternFromTranslate('/foo/{0}/{1}', [
