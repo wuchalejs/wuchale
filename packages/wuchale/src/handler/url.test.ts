03015cb1491920bbdd23a77a5b81b478311153b4
K1DV5 <kidusadugna@gmail.com>
2026-05-10 10:59:33 +0200

    test url handler methods

--- packages/wuchale/src/handler/url.test.ts
+++ packages/wuchale/src/handler/url.test.ts
@@ -1,6 +1,14 @@
 // $ node --import ../../testing/resolve.ts %f
 
-// import { test } from 'node:test'
-//
-// test('Pattern to/fro', t => {
-// })
+import { type TestContext, test } from 'node:test'
+import { URLHandler } from './url.js'
+
+const handler = new URLHandler(['en'], 'en', {
+    patterns: ['/bar/*'],
+})
+handler.initPatterns('foo', new Map())
+
+test('URL pattern match', (t: TestContext) => {
+    t.assert.deepStrictEqual(handler.match('/bar/foo'), [0, ['/foo']])
+    t.assert.deepStrictEqual(handler.matchToCompile('/bar/foo-{0}', 'en'), '/bar/foo-{0}')
+})
