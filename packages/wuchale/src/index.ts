8c91065920f93e29b63a6b431bdbc2a82426fc52
K1DV5 <kidusadugna@gmail.com>
2026-06-05 11:39:01 +0200

    export mergeItemsByKey from index, add changeset

--- packages/wuchale/src/index.ts
+++ packages/wuchale/src/index.ts
@@ -64,4 +64,4 @@ export type {
     StorageFactory,
     StorageFactoryOpts,
 } from './storage.js'
-export { defaultPluralRule, migrateStorage, storageByLocale, storageByType } from './storage.js'
+export { defaultPluralRule, mergeItemsByKey, migrateStorage, storageByLocale, storageByType } from './storage.js'
