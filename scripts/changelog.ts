14317f4baf17ba76ae3006ca3d07602fdd3c00c9
K1DV5 <kidusadugna@gmail.com>
2026-06-27 09:31:02 +0200

    changelog format: make commit sha link for website

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -1,8 +1,24 @@
-import git from '@changesets/changelog-git'
 import type { ChangelogFunctions } from '@changesets/types'
 
+const commitBaseUrl = 'https://github.com/wuchalejs/wuchale/commit/'
+
 export default {
-    getReleaseLine: git.getReleaseLine,
+    async getReleaseLine(changeset) {
+        let [firstLine, ...futureLines] = changeset.summary.split('\n').map(l => l.trimEnd())
+        if (firstLine?.startsWith('!')) {
+            firstLine = `⚠️ BREAKING: ${firstLine}`
+        }
+        let commitLinkPref = ''
+        if (changeset.commit) {
+            // for website changelog link
+            commitLinkPref = `[${changeset.commit.slice(0, 7)}](${commitBaseUrl}${changeset.commit}): `
+        }
+        let returnVal = `- ${commitLinkPref}${firstLine}`
+        if (futureLines.length > 0) {
+            returnVal += `\n${futureLines.map(l => `  ${l}`).join('\n')}`
+        }
+        return returnVal
+    },
     async getDependencyReleaseLine(changesets, dependenciesUpdated) {
         if (changesets.length === 0) {
             return ''
